75651d86a96028da489593855426c916d17b3e70
yugesh <a.yugesh2@gmail.com>
2025-01-23 00:38:10 +0530

    last commit before cia 1

---
+++ collage/python/set.py/remove.py
@@ -0,0 +1,9 @@
+parentSet = {1,2,3,4,5,6,7,8}
+
+parentSet.pop()
+
+print(parentSet)
+
+parentSet.remove(7)
+
+print(parentSet)
---
+++ collage/python/set.py/subset.py
@@ -0,0 +1,6 @@
+a = {1,2,3,4,5,6}
+
+b = {2,3,4}
+
+if b.issubset(a):
+    print("subSet is a subset of parentSet")
